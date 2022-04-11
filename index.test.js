4c3613ca16b6d77cd27b49f16bf610d65bc5b3aa
LukaszPiotrakArdigen <lukasz.piotrak@ardigen.com>
2022-04-11 09:39:11 +0200

    feat: add basic tests and solution

--- index.test.js
+++ index.test.js
@@ -1,17 +1,92 @@
-const isBingoBoardWinning = require("./index")
+import {BingoBoard} from './index';
 
-describe("Bingo Board", () => {
-    test("isBingoBoardWinning returns true for a winning board", ()=> {
-        let testWinningBingoBoard = [
-            [1,2,3,4,5],
-            [1,2,3,4,5],
-            [1,2,3,4,5],
-            [1,2,3,4,5],
-            [1,2,3,4,5],
-        ]
-        let testCalledNumbers = [1,2,3,4,5]
-        const result = isBingoBoardWinning(testWinningBingoBoard, testCalledNumbers)
-        const expected = true
-        expect(result).toEqual(expected)
-    })
-})
+test.each([
+    // Winning Rows
+    {
+        description: "first row is winning", board: [
+            [1, 2, 3, 4, 5],
+            [12, 22, 32, 42, 52],
+            [13, 23, 33, 43, 53],
+            [14, 24, 34, 44, 54],
+            [15, 25, 35, 45, 55]
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: true
+    },
+    {
+        description: "last row is winning", board: [
+            [11, 21, 31, 41, 51],
+            [12, 22, 32, 42, 52],
+            [13, 23, 33, 43, 53],
+            [14, 24, 34, 44, 54],
+            [1, 2, 3, 4, 5],
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: true
+    },
+    // Winning Columns
+    {
+        description: "first column is winning", board: [
+            [1, 12, 13, 14, 15],
+            [2, 22, 23, 24, 25],
+            [3, 32, 33, 34, 35],
+            [4, 42, 43, 44, 45],
+            [5, 52, 53, 54, 55]
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: true
+    },
+    {
+        description: "last column is winning", board: [
+            [11, 21, 31, 41, 1],
+            [12, 22, 32, 42, 2],
+            [13, 23, 33, 43, 3],
+            [14, 24, 34, 44, 4],
+            [15, 25, 35, 45, 5]
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: true
+    },
+    // Winning Row and Column
+    {
+        description: "row and column are winning", board: [
+            [11, 21, 31, 41, 1],
+            [12, 22, 32, 42, 2],
+            [13, 23, 33, 43, 3],
+            [14, 24, 34, 44, 4],
+                [1, 2, 3, 4, 5],
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: true
+    },
+    // Losing Board
+    {
+        description: "last column has 4 called", board: [
+            [11, 21, 31, 41, 1],
+            [12, 22, 32, 42, 2],
+            [13, 23, 33, 43, 3],
+            [14, 24, 34, 44, 4],
+            [15, 25, 35, 45, 55]
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: false
+    },
+    {
+        description: "last column has 4 called", board: [
+            [11, 21, 31, 41, 1],
+            [12, 22, 32, 42, 2],
+            [13, 23, 33, 43, 3],
+            [14, 24, 34, 44, 4],
+            [1, 2, 3, 4, 55],
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: false
+    },
+    // Invalid input
+    {
+        description: "last column has 4 called", board: [
+            [11, 21, 31, 41, 51],
+            [12, 22, 32, 42, 52],
+            [13, 23, 33, 43, 53],
+            [14, 24, 34, 44, 54],
+            [1, 2, 3, 4, 55],
+
+        ], calledNumbers: [1, 2, 3, 4, 5], expected: false
+    },
+])('returns $expected when $description', ({description, board, calledNumbers, expected}) => {
+    const bingoBoard = new BingoBoard(board)
+    expect(bingoBoard.isBingo(calledNumbers)).toEqual(expected)
+});
--- index.ts
+++ index.ts
@@ -1,27 +1,77 @@
-function initializeHits() {
-    let hits = Array(5)
-    Array(5).forEach((_, i) => {
-        hits[i] = new Array(false, 5)
-    })
-    return hits;
-}
+export class BingoBoard {
+    public static rowsCount = 5;
+    public static columnsCount = 5;
 
- function isBingoBoardWinning(bingoBoard, calledNumbers) {
-     let hits = initializeHits();
-     calledNumbers.forEach(calledNumber => {
-        bingoBoard.forEach((row, rowNumber) => {
-            row.forEach((bingoNumber, colNumber) => {
-                if (calledNumber === bingoNumber) {
-                    hits[rowNumber][colNumber] = true
-                    if (hits[rowNumber].every(value => Boolean(value))) {
-                        return true
-                    }
+    private numbers: number[][];
+
+    constructor(numbers: number[][]) {
+        this.validateNumbers(numbers);
+        this.numbers = numbers;
+    }
+
+    validateNumbers(numbers: number[][]) {
+        if (numbers.length !== BingoBoard.rowsCount) {
+            throw new Error(`Invalid rows count. Was: ${numbers.length}, expected: ${BingoBoard.rowsCount}`);
+        }
+
+        numbers.forEach(row => {
+            if (row.length !== BingoBoard.columnsCount) {
+                throw new Error(`Invalid columns count. Was: ${row.length}, expected: ${BingoBoard.columnsCount}`);
+                // Could also validate if there are no duplicate numbers in the board. 
+                // Choosing not do do that since it's not a requirement and would cause significant overhead.
+            }
+        });
+    }
+
+    public numberAt(column: number, row: number): number {
+        if (column < 0 || column >= BingoBoard.columnsCount) throw new Error("Column out of range");
+        if (row < 0 || row >= BingoBoard.rowsCount) throw new Error("Row out of range");
+        return this.numbers[row][column];
+    }
+
+    public isBingo(calledNumbers: number[]): boolean {
+        let hits = new BingoBoardHits(BingoBoard.rowsCount, BingoBoard.columnsCount)
+        let calledNumbersSet = new Set(calledNumbers);
+
+        for (const [rowNumber, row] of this.numbers.entries()) {
+            for (const [columnNumber, bingoNumber] of row.entries()) {
+                if (calledNumbersSet.has(bingoNumber)) {
+                    if (hits.markHit(rowNumber, columnNumber)) return true;
                 }
-            })
-        })
-    })
-    return false;
+            }
+        }
+        return false
+    }
 }
 
+class BingoBoardHits {
+    private rowHits: number[];
+    private columnHits: number[];
+    private winningRow: number | null = null;
+    private winningColumn: number | null = null;
 
-module.exports = isBingoBoardWinning
+    constructor(rowsCount: number, columnsCount: number) {
+        if (rowsCount < 0) throw new Error("rowsCount cannot be negative");
+        if (columnsCount < 0) throw new Error("columnsCount cannot be negative");
+        this.rowHits = Array(rowsCount).fill(0);
+        this.columnHits = Array(columnsCount).fill(0);
+    }
+
+    public markHit(row: number, column: number): boolean {
+        if (++this.rowHits[row] >= BingoBoard.rowsCount) {
+            this.winningRow = row;
+            return true;
+        }
+
+        if (++this.columnHits[column] >= BingoBoard.columnsCount) {
+            this.winningColumn = column;
+            return true;
+        }
+
+        return false;
+    }
+
+    public isBingo(): boolean {
+        return this.winningRow !== null || this.winningColumn !== null;
+    }
+}
